7ab249e3a4f6366cf0e702da82ebc637ee6d1687
wpeery <peery@umail.ucsb.edu>
2018-04-05 19:56:20 -0700

    adds food report types

--- Types.go
+++ Types.go
@@ -1,20 +1,21 @@
 package main
 
 // Food Search Types
+
 type FoodSearch struct {
 	List Info `json:"list"`
 }
 
 type Info struct {
-	Q     string           `json: "q"`
-	Sr    string           `json: "sr"`
-	Ds    string           `json: "ds"`
-	Start int              `json: "start"`
-	End   int              `json: "end"`
-	Total int              `json: "total"`
-	Group string           `json: "group"`
-	Sort  string           `json: "sort"`
-	Item  []FoodSearchItem `json: "item"`
+	Query           string           `json:"q"`
+	StandardRelease string           `json:"sr"`
+	DatabaseSource  string           `json:"ds"`
+	Start           int              `json:"start"`
+	End             int              `json:"end"`
+	Total           int              `json:"total"`
+	Group           string           `json:"group"`
+	Sort            string           `json:"sort"`
+	Item            []FoodSearchItem `json:"item"`
 }
 
 type FoodSearchItem struct {
@@ -26,19 +27,54 @@ type FoodSearchItem struct {
 }
 
 // Food Report Types
+
 type FoodReport struct {
+	Foods    []Food  `json:"foods"`
+	Count    int     `json:"count"`
+	NotFound int     `json:"notfound"`
+	API      float32 `json:"api"`
 }
 
-// fmt.Println("Q: ", search.List.Q)
-// fmt.Println("SR: ", search.List.Sr)
-// fmt.Println("DS: ", search.List.Ds)
-// fmt.Println("Start: ", search.List.Start)
-// fmt.Println("End: ", search.List.End)
-// fmt.Println("Total: ", search.List.Total)
-// fmt.Println("Group: ", search.List.Group)
-// fmt.Println("Sort: ", search.List.Sort)
-// fmt.Println("item[0].Offset: ", search.List.Item[0].Offset)
-// fmt.Println("item[0].Group: ", search.List.Item[0].Group)
-// fmt.Println("item[0].Name: ", search.List.Item[0].Name)
-// fmt.Println("item[0].NDBNO: ", search.List.Item[0].NDBNO)
-// fmt.Println("item[0].DataSource: ", search.List.Item[0].DataSource)
+type Food struct {
+	StandardRelease string `json:"sr"`
+	Type            string
+	Description     []FoodDesc `json:"desc"`
+	Nutrients       []Nutrient
+}
+
+type FoodDesc struct {
+	NDBNO              string
+	Name               string
+	ShortDescription   string `json:"sd"`
+	FoodGroup          string `json:"fg"`
+	ScientificName     string `json:"sn"`
+	CommercialName     string `json:"cn"`
+	Manufacturer       string `json:"manu"`
+	NitrogenFactor     int    `json:"nf"`
+	CarbohydrateFactor int    `json:"cf"`
+	FatFactor          int    `json:"ff"`
+	ProteinFactor      int    `json:"pf"`
+	RefusePercent      string `json:"r"`
+	RefuseDescription  string `json:"rd"`
+	DatabaseSource     string `json:"ds"`
+	ReportingUnit      string `json:"ru"`
+}
+
+type Nutrient struct {
+	NutrientID    int `json:"nutrient_id"`
+	Name          string
+	Group         string
+	Unit          string
+	Value         float32
+	Derivation    string
+	DataPoints    int    `json:"dp"`
+	StandardError string `json:"se"`
+}
+
+type Measure struct {
+	Label      string
+	Equivalent float32 `json:"eqv"`
+	Eunit      string  `json:"eunit"`
+	Quantity   int     `json:"qty"`
+	Value      float32
+}
